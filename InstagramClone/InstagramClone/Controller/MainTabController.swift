04f194c49b5c3c4eeee7e8788018331230781e0b
Nanda <nanda.perdana@kuncie.com>
2024-07-15 04:11:17 +0800

    Initial Commint

---
+++ InstagramClone/InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by nandawperdana on 15/07/24.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+    }
+}
