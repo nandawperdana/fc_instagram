b9445725c3fb3d72c428fb2cac129ca1c11d590c
Nanda <nanda.perdana@kuncie.com>
2024-08-04 22:53:10 +0700

    implement edit profile

--- InstagramClone/InstagramClone/API/UserService.swift
+++ InstagramClone/InstagramClone/API/UserService.swift
@@ -7,6 +7,7 @@
 
 import FirebaseFirestore
 import FirebaseAuth
+import FirebaseStorage
 
 typealias FirestoreCompletion = (Error?) -> Void
 
@@ -71,4 +72,44 @@ class UserService {
             }
         }
     }
+    
+    func updateProfileImage(forUser user: User, image: UIImage, completion: @escaping(String?, Error?) -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        Storage.storage().reference(forURL: user.profileImage).delete(completion: nil)
+                
+        ImageUploaderService.shared.uploadImage(image: image) { profileImageUrl in
+            let data = ["profileImage": profileImageUrl]
+            
+            FirebaseReference.getReference(.User).document(uid).updateData(data) { error in
+                if let error = error {
+                    completion(nil, error)
+                    return
+                }
+                
+                FirebaseReference.getReference(.Post).whereField("ownerUid", isEqualTo: user.uid).getDocuments { snapshot, error in
+                    guard let documents = snapshot?.documents else { return }
+                    let data = ["ownerProfileImage": profileImageUrl]
+                    documents.forEach({ FirebaseReference.getReference(.Post).document($0.documentID).updateData(data) })
+                }
+                
+                // need to update profile image url in comments and messages
+                
+                completion(profileImageUrl, nil)
+            }
+        }
+    }
+    
+    func saveUserData(user: User, completion: @escaping(FirestoreCompletion)) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let data: [String: Any] = ["email": user.email,
+                                   "fullname": user.fullname,
+                                   "profileImage": user.profileImage,
+                                   "uid": uid,
+                                   "username": user.username]
+        
+        FirebaseReference.getReference(.User).document(uid).setData(data, completion: completion)
+    }
+    
 }
