f032efc73a1b32187420392c0375d934945dd234
Nanda <perdana.nandha@gmail.com>
2024-08-05 18:08:43 +0700

    test: create unit test and ci/cd script (#11)
    * implement unit test on model and auth view model
    
    * create fastfile script configuration
    
    * create github actions script
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml
    
    * update ios_cd.yml
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml
    
    * Update ios_cd.yml

--- InstagramClone/Pods/Target Support Files/Pods-InstagramClone-InstagramCloneUITests/Pods-InstagramClone-InstagramCloneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InstagramClone_InstagramCloneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_InstagramClone_InstagramCloneUITests
-@end
